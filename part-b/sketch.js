2e4da5df1d164b9c4173881ef3995e63d063918b
Aweller95 <aliweller68@hotmail.com>
2019-04-02 12:00:49 +0100

    Removed parts a and b. Initial edits to part C

--- part-b/sketch.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-  createCanvas(400, 400)
-  background(0, 0, 0)
-}
-
-function draw() {
-  fill(255, 0, 0)
-  ellipse(mouseX, mouseY, 30, 30)
-}
--- part-c/sketch.js
+++ part-c/sketch.js
@@ -1,14 +1,49 @@
+var r = 0;
+var g = 0;
+var b = 0;
+var resetButton = document.getElementById('reset')
+
+resetButton.addEventListener('click', reset(), false);
+
+function reset () {
+    if (r>0 )
+  r = 0;
+  document.getElementById('red').textContent = r;
+
+  g = 0;
+  document.getElementById('green').textContent = g;
+
+  b = 0;
+  document.getElementById('blue').textContent = b;
+
+}
+
+
+
+
 function setup() {
-  createCanvas(400, 400)
+  createCanvas(600, 600)
   background(0, 0, 0)
 }
 
-function draw() {
-  if (mouseIsPressed) {
-    fill(255, 0, 0)
-  } else {
-    fill(0, 255, 0)
+function keyTyped() {
+  if (key === 'r') {
+    r += 5;
+    document.getElementById('red').textContent = r;
+  } else if (key === 'g') {
+    g += 5;
+    document.getElementById('green').textContent = g;
+  } else if (key === 'b') {
+    b += 5;
+    document.getElementById('blue').textContent = b;
   }
+  
+  return false; // prevent default
+}
+
+function draw() {
+ 
+  fill(r, g, b)
 
   ellipse(mouseX, mouseY, 30, 30)
 }
